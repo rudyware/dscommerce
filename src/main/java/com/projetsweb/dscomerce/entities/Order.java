e9afe8db1b4c35f2a479e20086e1af3bbbb5eec1
Rudy Carvalho <rudyvaldano@gmail.com>
2025-09-11 22:15:03 +0100

    adding entity order

---
+++ src/main/java/com/projetsweb/dscomerce/entities/Order.java
@@ -0,0 +1,55 @@
+package com.projetsweb.dscomerce.entities;
+
+import com.projetsweb.dscomerce.enums.OrderStatus;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus orderStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {}
+
+    public Order(Long id, Instant moment, OrderStatus orderStatus) {
+        this.id = id;
+        this.moment = moment;
+        this.orderStatus = orderStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+}
---
+++ src/main/java/com/projetsweb/dscomerce/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.projetsweb.dscomerce.enums;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}
